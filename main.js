fc9f9503de7e8d603a4104cc2a5da28792e261af
jaodograu444 <joao.lemos.quadros@escola.pr.gov.br>
2024-09-26 08:46:56 -0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "joao Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent;
+ 
+ var tdImc = paciente.querySelector(".info-imc");
+ var imc = peso / (altura * altura);
+ 
+
+tdImc.textContent = imc;
+
+if (peso < 0 ) {
+   tdPeso.textContent = "Peso inválido!";
+ } 
+
+
