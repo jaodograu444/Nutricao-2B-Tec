9795b51bf9b80f2ec6e733db8ecc9fcc90ced952
jaodograu444 <joao.lemos.quadros@escola.pr.gov.br>
2024-09-27 12:05:55 -0300

    update

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "joao Nutrição";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < 5; i ++) {
+  console.log(pacientes[i]);
+  
+  var tdPeso = pacientes[i].querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura")
+var tdAltura = pacientes[i].querySelector(".info-altura");
 var altura = tdAltura.textContent;
  
- var tdImc = paciente.querySelector(".info-imc");
- var imc = peso / (altura * altura);
+ var tdImc = pacientes[i].querySelector(".info-imc");
+
  
+console.log(peso);
+console.log(altura);
 
-tdImc.textContent = imc;
-
-if (peso < 0 ) {
-   tdPeso.textContent = "Peso inválido!";
- } 
+var pesoEhValido = true;
+var alturaEhValida = true;
 
 
+if(peso < 0 || peso > 600) {
+   tdImc.textContent = "Peso inválido!";
+   pesoEhValido = false;
+ }
+ 
+if(altura < 0 || altura > 2.80) {
+  tdImc.textContent = "altura inválida!";
+  alturaEhValida = false;
+}
+
+if (pesoEhValido && alturaEhValida) {
+ var imc = peso / (altura * altura);
+  tdImc.textContent = imc;1
+}
+
+}
